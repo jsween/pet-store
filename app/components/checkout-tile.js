73f83730b66855bcec5abed7d8276a036e149cec
Aundra Miller and Jon Sweeney <jonsweeney26@gmail.com>
2016-04-07 16:40:53 -0700

    add a button to remove item from checkout crate; need to fix bug to update total

--- app/components/checkout-tile.js
+++ app/components/checkout-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCrate: Ember.inject.service(),
+  actions: {
+    removeAnimalFromCart(animal) {
+      this.get('shoppingCrate').remove(animal);
+    }
+  }
 });
--- app/routes/checkout.js
+++ app/routes/checkout.js
@@ -2,16 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   shoppingCrate: Ember.inject.service(),
-  // displayTotal: Ember.computed('shoppingCrate.animals.[]', function() {
-  //   var total = 8;
-  //   var animalArray = shoppingCrate.animals;
-  //   animalArray.forEach(function(animal) {
-  //     debugger;
-  //     total += 5;
-  //   });
-  //   console.log(total);
-  //   return total;
-  // }),
+
   model() {
     return this.get('shoppingCrate').get('animals');
   },
