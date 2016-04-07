1572d5307f7ec6424dfb08f977d0930772d468cb
Aundra Miller and Jon Sweeney <jonsweeney26@gmail.com>
2016-04-07 13:09:29 -0700

    successfully inject service into route

--- app/routes/checkout.js
+++ app/routes/checkout.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  shoppingCrate: Ember.inject.service()
+  shoppingCrate: Ember.inject.service(),
+  model() {
+    return this.get('shoppingCrate').get('animals');
+  }
 });
