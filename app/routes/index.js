f0b78f41555ca048b102fc307d9e64cc09507f1d
Adam Ross Russel <Guest@epicodus-34.local>
2016-04-08 14:46:10 -0700

    added admin verification

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('admin');
+  },
+  actions: {
+    checkAdmin(model) {
+      var usernameAttempt = prompt("Please enter username");
+      var passwordAttempt = prompt('Please enter your password');
+
+      var self = this;
+      model.forEach(function(administrator) {
+        if (administrator.get('username') === usernameAttempt && administrator.get('password') === passwordAttempt) {
+          self.transitionTo('admin');
+          return alert("Your credentials have been verified!");
+        } else {
+        self.transitionTo('index');
+        return alert('Username or Password are incorrect.');
+      }
+      });
+    }
+  }
 });
