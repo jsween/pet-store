717df88140c3a49c08893185d5ae91edb915fb17
Tal Haberer <talhaberer2014@gmail.com>
2016-04-12 16:56:04 -0700

    tal commit

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('animal');
+  },
   actions: {
     saveAnimalToStore(params) {
       var animal = this.store.createRecord('animal', params);
       animal.save();
+      this.transitionTo('admin');
+    },
+    destroyAnimal(animal) {
+      animal.destroyRecord(),
       this.transitionTo('admin');
     }
   }
