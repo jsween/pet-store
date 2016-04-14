9f3096da46f4c3aae6e1905b948cec439cb78627
Jon <jonsweeney26@gmail.com>
2016-04-14 08:40:50 -0700

    add helper to calculate year or years old

---
+++ tests/unit/helpers/calc-years-test.js
@@ -0,0 +1,10 @@
+import { calcYears } from 'e-commerce/helpers/calc-years';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | calc years');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = calcYears([42]);
+  assert.ok(result);
+});
